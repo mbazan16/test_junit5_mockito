6bf9f6ca5d202ad3ed6326abd0e84b95ddba09c4
scheng <scheng@ITEM-S131206.emea.msad.sopra>
2022-10-04 17:36:41 +0200

    Añadido el método de exception de división

---
+++ junit5_mockito/excepciones/DivisorCeroExcepcion.java
@@ -0,0 +1,11 @@
+package com.test.ejemplos.excepciones;
+
+public class DivisorCeroExcepcion extends RuntimeException {
+
+	public DivisorCeroExcepcion(String message) {
+		super(message);		
+	}
+	
+	
+
+}
--- junit5_mockito/src/test/java/com/test/ejemplos/DividirTest.java
+++ junit5_mockito/src/test/java/com/test/ejemplos/DividirTest.java
@@ -1,6 +1,7 @@
 package com.test.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.test.ejemplos.excepciones.DivisorCeroExcepcion;
 import com.test.ejemplos.interfaces.Calcular;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,4 +39,17 @@ private final static Logger log= Logger.getLogger("com.test.ejemplos.Calculadora
 		assertEquals(1, this.dividir.dividir(1,1), "1 / 1 should equal 1");
 	}
 	
+	/*@Test
+	@DisplayName("dividir dos operandos resultado Float")
+	public void dividirDosOperandosResultadoFloat(){
+		when(calcular.operar(3,2)).thenReturn(1.5);
+		assertEquals(1.5, this.dividir.dividir(3,2), "3 / 2 should equal 1.5 ");
+	}*/
+	
+	@Test
+	@DisplayName("Division por cero")
+	public void divisionPorCero() {
+		when(calcular.operar(1,0)).thenThrow(new DivisorCeroExcepcion("Division por cero"));
+		Exception exception = assertThrows(DivisorCeroExcepcion.class, ()-> this.dividir.dividir(1,0));
+	}
 }
