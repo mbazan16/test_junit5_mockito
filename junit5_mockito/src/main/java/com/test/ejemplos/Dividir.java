8aeaadcc84853110ea28093450711f8faec00852
dmeyobarros <dmeyobarros@AAP-5CG2161V0T>
2022-10-04 17:29:13 +0200

    Cambio para dividir

---
+++ junit5_mockito/src/main/java/com/test/ejemplos/Dividir.java
@@ -0,0 +1,19 @@
+package com.test.ejemplos;
+
+import com.test.ejemplos.interfaces.Calcular;
+
+public class Dividir {
+	
+	Calcular calcular;
+	
+	/*Cuando usamos stub o "mockito" manual
+	public Sumar(Calcular calcular) {
+		this.calcular=calcular;
+	}*/
+	 	
+	public int dividir(int... operandos) {
+	
+		return calcular.operar(operandos);
+	}
+
+}
---
+++ junit5_mockito/src/test/java/com/test/ejemplos/DividirTest.java
@@ -0,0 +1,51 @@
+package com.test.ejemplos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.test.ejemplos.interfaces.Calcular;
+
+
+
+@ExtendWith(MockitoExtension.class)
+class DividirTest {
+	private final static Logger log= Logger.getLogger("com.test.ejemplos.CalculadoraTest");
+	
+	@InjectMocks
+	Dividir dividir= new Dividir(); //IMPORTANTE: General la instancia cuando utilizamos inyecci�n
+	/*
+	//Stub o mockito
+	Calcular calcular = (int... operandos)->{return operandos[0]+operandos[1];};
+	*/
+	
+	@Mock Calcular calcular;
+	@BeforeEach
+	void init() {	    
+	    //this.dividir= new Dividir(calcular); //Cuando usamos stub o "mockito" manual
+	    log.info("Ejecutamos test");
+	}
+	
+	@Test
+	@DisplayName("Division dos operandos")
+	public void dividirPorCero(){
+		when(calcular.operar(1,1)).thenReturn(1);
+		assertEquals(1, this.dividir.dividir(1,1), "1 / 1 should equal 1");
+	}
+	
+	
+	
+	
+}
+
+
